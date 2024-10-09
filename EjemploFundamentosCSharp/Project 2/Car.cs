e54500c693b22ac05860d738d332cd372290e4aa
Jabuitron <Jandresbuiitron@gmail.com>
2024-10-09 16:34:20 -0500

    Se creo la clase carro y formulario

---
+++ EjemploFundamentosCSharp/Project 2/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_2
+{
+    public class Car
+    {
+        string color;
+        int cinlindraje;
+        int modelo;
+
+        public string stop()
+        {
+            string msj = "El Carro está detenido";
+            return msj;
+        }
+    }
+}
---
+++ EjemploFundamentosCSharp/Project 2/index.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project_2
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            mostrarMensaje();
+        }
+        // Método para mostrar el mensaje por el navegador
+        public void mostrarMensaje()
+        {
+            Car car = new Car();
+            string msj = car.stop();
+            lblmsj.Text = msj;
+        }
+    }
+}
